45284669d2fea7d4fee2e11a7e23343ccae8b95f
Priyanshu Joshi <priyanshujoshi2252.com>
2026-05-07 18:10:04 +0530

    Fix navigation logo path, static loading order, and absolute media paths

--- mac/settings.py
+++ mac/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Use WhiteNoise's storage for production
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
